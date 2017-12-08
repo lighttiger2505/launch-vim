0c51d94af7af67dce58e0397f073480ba8f97bb7
lighttiger <lighttiger2505@gmail.com>
2017-12-09 00:42:28 +0900

    Remove except only function of opening vim and create temp file

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"runtime"
-	"strings"
 )
 
 func main() {
@@ -20,16 +15,9 @@ func main() {
 }
 
 func launchVim() int {
-	message := `# |<----  Opened the file with your favorite editor. The first block of text is the title.  ---->|
-
-
-# |<----  The following blocks are explanations  ---->|
-
-`
-
 	// Make temp editing file
 	fPath := getFilePath("ISSUE")
-	err := makeFile(fPath, message)
+	err := makeFile(fPath)
 	if err != nil {
 		fmt.Fprint(os.Stdout, fmt.Sprintf("failed make edit file. %s\n", err.Error()))
 		return 1
@@ -37,7 +25,7 @@ func launchVim() int {
 	defer deleteFile(fPath)
 
 	// Open text editor
-	err = openEditor("vim", "--cmd", "set ft=gitcommit tw=0 wrap lbr", fPath)
+	err = openEditor("vim", fPath)
 	if err != nil {
 		fmt.Fprint(os.Stdout, fmt.Sprintf("failed open text editor. %s\n", err.Error()))
 		return 1
@@ -50,15 +38,7 @@ func launchVim() int {
 		return 1
 	}
 
-	// Parce read content
-	reader := bytes.NewReader(content)
-	title, body, err := perseTitleAndBody(reader, "#")
-	if err != nil {
-		fmt.Fprint(os.Stdout, fmt.Sprintf("failed parce content. %s\n", err.Error()))
-		return 1
-	}
-
-	fmt.Fprint(os.Stdout, fmt.Sprintf("title=%s, body=%s\n", title, body))
+	fmt.Fprint(os.Stdout, string(content))
 
 	return 0
 }
@@ -72,10 +52,9 @@ func getFilePath(about string) string {
 	return fname
 }
 
-func makeFile(fPath, message string) (err error) {
-	// only write message if file doesn't exist
-	if !isFileExist(fPath) && message != "" {
-		err = ioutil.WriteFile(fPath, []byte(message), 0644)
+func makeFile(fPath string) (err error) {
+	if !isFileExist(fPath) {
+		err = ioutil.WriteFile(fPath, []byte(""), 0644)
 		if err != nil {
 			return
 		}
@@ -98,35 +77,4 @@ func openEditor(program string, args ...string) error {
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
-}
-
-func perseTitleAndBody(reader io.Reader, cs string) (title, body string, err error) {
-	var titleParts, bodyParts []string
-
-	r := regexp.MustCompile("\\S")
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.HasPrefix(line, cs) {
-			continue
-		}
-
-		if len(bodyParts) == 0 && r.MatchString(line) {
-			titleParts = append(titleParts, line)
-		} else {
-			bodyParts = append(bodyParts, line)
-		}
-	}
-
-	if err = scanner.Err(); err != nil {
-		return
-	}
-
-	title = strings.Join(titleParts, " ")
-	title = strings.TrimSpace(title)
-
-	body = strings.Join(bodyParts, "\n")
-	body = strings.TrimSpace(body)
-
-	return
 }
